9b26a130404fc8b17ad17059c02f648ed2f9a7bb
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2021-02-19 03:02:12 +0530

    add pre commit hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
 
-app.use(express.json)
-
-
+app.use(express.json())
+app.use(cors())
