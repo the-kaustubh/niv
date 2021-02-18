c5a665df67b9db97e531b23bb82920093f1b22b1
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2021-02-19 03:08:57 +0530

    Add mongoose connection

--- src/index.js
+++ src/index.js
@@ -5,3 +5,16 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect(
+	process.env.DATABASE_URL,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	});
+
+const db = mongoose.connection;
+db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("Connected to Database"));
