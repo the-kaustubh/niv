197c5e8a018880999f85d36d8986c04bf440c171
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2021-03-26 20:00:38 +0530

    Add way to add datetime

--- src/routes/write.js
+++ src/routes/write.js
@@ -4,9 +4,12 @@ const Reading = require('../models/readings')
 
 router.post('/reading', async (req, res) => {
   try {
+    if (req.body.backup === true) {
+      const dt = req.body.datetime * 1000
+      req.body.datetime = new Date(dt)
+    }
     const reading = new Reading(req.body)
-    const savedReading = reading.save()
-    console.log(savedReading)
+    const savedReading = await reading.save()
     if (savedReading == null) {
       throw new Error('Could not save.')
     }
