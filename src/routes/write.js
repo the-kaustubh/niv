57e689e92de08421a6196612037c872aee2e9fc3
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2022-01-16 03:18:21 +0530

    Modify error msg make more friendly

--- src/routes/write.js
+++ src/routes/write.js
@@ -29,7 +29,7 @@ router.post('/reading', async (req, res) => {
     )
 
     if (updatedNode == null) {
-      throw new Error('Could not save.' + req.body.uid)
+      throw new Error('Could not save - ' + req.body.uid + '. Please check if node exists')
     }
 
     res.status(201).json(
