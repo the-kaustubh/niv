a4e3d78707afadd484e01af7a6cab26c20666e53
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2022-01-26 19:34:01 +0530

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16.10.0
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY package*.json ./
 
--- src/updates/checkUpstream.js
+++ src/updates/checkUpstream.js
@@ -2,20 +2,26 @@ const { execFile } = require('child_process')
 
 async function checkForUpdates () {
   try {
-    await runCmd('git', ['fetch']).then(so => console.log('f', so))
-    let so = await runCmd('git', ['status', '-sb'])
-    so = so
-      .trim()
-      .split('\n')[0]
-      .search(/behind/)
-    console.log(so)
-    if (so === -1) {
-      console.log('no updates found')
-      return
-    }
-    await runCmd('git', ['pull'])
-    await runCmd('rm', ['/app/logs/*', '-f'])
-    await runCmd('pm2', ['restart', 'index'])
+    await runCmd('/home/kaustubh/Documents/JS/ates/niv/get-update.sh', ['latest', '/home/kaustubh/Desktop/lol'])
+    let exePath = await runCmd('pwd')
+    exePath = exePath.split('\n')[0]
+
+    runCmd('file', [`${exePath}/niv`]).then(so => console.log(so))
+
+    // await runCmd('git', ['fetch']).then(so => console.log('f', so))
+    // let so = await runCmd('git', ['status', '-sb'])
+    // so = so
+    //   .trim()
+    //   .split('\n')[0]
+    //   .search(/behind/)
+    // console.log(so)
+    // if (so === -1) {
+    //   console.log('no updates found')
+    //   return
+    // }
+    // await runCmd('git', ['pull'])
+    // await runCmd('rm', ['/app/logs/*', '-f'])
+    // await runCmd('pm2', ['restart', 'index'])
   } catch (er) {
     console.log(er)
   }
