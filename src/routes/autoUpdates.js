c08e0b3e6c721647181c87d9f85b8de7855e1512
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2022-01-26 23:57:19 +0530

    Fix update error

--- src/routes/autoUpdates.js
+++ src/routes/autoUpdates.js
@@ -2,14 +2,14 @@ const express = require('express')
 const router = express.Router()
 const { checkForUpdatesServer, checkForUpdatesClient } = require('../updates/checkUpstream')
 
-router.get('/server/:token', async (_req, res) => {
+router.get('/server/:token', async (req, res) => {
   const token = req.params.token
   console.log(token)
   checkForUpdatesServer(token)
   res.json({ msg: 'Checking for updates' })
 })
 
-router.get('/client/:token', async (_req, res) => {
+router.get('/client/:token', async (req, res) => {
   const token = req.params.token
   console.log(token)
   checkForUpdatesClient(token)
